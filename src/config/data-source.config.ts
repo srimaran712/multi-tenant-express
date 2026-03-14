0aa7bd0ec83d973c8483ceaadcbd547060120b73
Manimaran Srinivasan <srimaran712@gmail.com>
2026-03-14 18:38:42 +0530

    fixing db connection with rds

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -2,17 +2,24 @@ import {DataSource} from "typeorm";
 import { User } from "../models/user.model";
 import { Tenant } from "../models/tenant.model";
 import { Project } from "../models/project.model";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5433,
-    username: "postgres",
-    password: "Remember001",
-    database: "multi-tenant",
-    synchronize: true,
+    host: process.env.DB_HOST as string,
+    port: parseInt(process.env.DB_PORT as string),
+    username: process.env.DB_USERNAME as string,
+    password: process.env.DB_PASSWORD as string,
+    database: process.env.DB_NAME as string,
+    synchronize: false,
     logging: false,
     entities: [User, Tenant, Project],
     migrations: [],
     subscribers: [],
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
